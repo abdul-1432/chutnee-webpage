a9f967638ed1af6a756d478cab8f0a475dc4c230
Mohammad Abdul Gafoor <gafoor.mngr@gmail.com>
2025-10-26 23:42:00 +0530

    Updated site

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/chutnee-webpage/", // 👈 Add this line for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
